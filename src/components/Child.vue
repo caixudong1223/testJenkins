2d9bb6194021316c36980593607dbbce98e73148
caixudong1223 <cxdazxm@163.com>
2018-04-18 10:57:49 +0800

    测试前端自动化部署jenkins

---
+++ src/components/Child.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="child">
+      <p>子组件</p>
+      <p>{{count}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+      return {
+          
+      }
+  },
+  props: ['count']
+}
+</script>
+
+
+<style scoped>
+    .child{
+        border: 1px solid green;
+        width: 80%;
+        margin: 0 auto;
+    }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <router-link to="/parent/child">子组件/child</router-link>
+    <h1>{{ msg }}</h1>
+    <p>{{count}}</p>
+    <router-view :count="count"></router-view>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '父组件',
+      count: 0
+    }
+  },
+  created(){
+    this.setTimer()
+  },
+  mounted(){
+    
+  },
+  methods: {
+    setTimer(){
+      setInterval(() => {  
+        this.count++;
+      }, 1000)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+.hello{
+  border: 1px solid #ccc;
+}
+
+</style>
